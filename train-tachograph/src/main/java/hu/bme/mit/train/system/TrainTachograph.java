910e14c1f3a9bb7451da8cf3cee04ff3e45d92dc
tiborkott <38768467+tiborkott@users.noreply.github.com>
2021-03-01 21:18:16 +0100

    Commit Add external dependencies 6.4 Google Guava Library

---
+++ train-tachograph/src/main/java/hu/bme/mit/train/system/TrainTachograph.java
@@ -0,0 +1,10 @@
+package hu.bme.mit.train.system;
+import com.google.common.collect.HashBasedTable;
+import com.google.common.collect.Table;
+
+import java.util.List;
+
+public class TrainTachograph {
+
+    Table<Integer, Integer, Integer> Tachograph = HashBasedTable.create();
+}
